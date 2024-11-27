72a1ea01e77fd76351134c5326f327fe6e2d8da9
hazywho <116442947+hazywho@users.noreply.github.com>
2024-11-27 08:07:45 +0800

    Update motorsystemGPIOPartly.py

--- FIRMATA+RPI/motorsystemGPIOPartly.py
+++ FIRMATA+RPI/motorsystemGPIOPartly.py
@@ -1,133 +1,155 @@
-import pyfirmata2
+from gpiozero.pins.pigpio import PiGPIOFactory
 import time
+import math
+from gpiozero import Servo
+import RPi.GPIO as GPIO
 
-class board():
-
+class car():
     def stop(self):
-        self.leftM1B.write(0)
-        self.leftM2B.write(0)
-        self.rightM1B.write(0)
-        self.rightM2B.write(0)
+        GPIO.output(self.leftM1APin,0)
+        GPIO.output(self.leftM2APin,0)
+        GPIO.output(self.rightM1APin,0)
+        GPIO.output(self.rightM2APin,0)
         
-        self.leftM1A.write(0)
-        self.leftM2A.write(0)
-        self.rightM1A.write(0)
-        self.rightM2A.write(0)
-    
+        GPIO.output(self.leftM1BPin,0)
+        GPIO.output(self.leftM2BPin,0)
+        GPIO.output(self.rightM1BPin,0)
+        GPIO.output(self.rightM2BPin,0)
+
     def front(self,delay=5,willStop=True):
-        self.leftM1A.write(1)
-        self.leftM2A.write(1)
-        self.rightM1A.write(1)
-        self.rightM2A.write(1)
+        GPIO.output(self.leftM1APin,1)
+        GPIO.output(self.leftM2APin,1)
+        GPIO.output(self.rightM1APin,1)
+        GPIO.output(self.rightM2APin,1)
+
         time.sleep(delay)
         self.stop() if willStop else None
-    
+
     def back(self,delay=5,willStop=True):
-        self.leftM1B.write(1)
-        self.leftM2B.write(1)
-        self.rightM1B.write(1)
-        self.rightM2B.write(1)
+        GPIO.output(self.leftM1BPin,1)
+        GPIO.output(self.leftM2BPin,1)
+        GPIO.output(self.rightM1BPin,1)
+        GPIO.output(self.rightM2BPin,1)
+
         time.sleep(delay)
         self.stop() if willStop else None
-        
+
     def left(self,delay=5,willStop=True):
-        self.leftM1B.write(1)
-        self.leftM2A.write(1)
-        self.rightM1B.write(1)
-        self.rightM2A.write(1)
+        GPIO.output(self.leftM1BPin,1)
+        GPIO.output(self.leftM2BPin,1)
+        GPIO.output(self.rightM1APin,1)
+        GPIO.output(self.rightM2APin,1)
+
         time.sleep(delay)
         self.stop() if willStop else None
 
     def right(self,delay=5,willStop=True):
-        self.leftM1A.write(1)
-        self.leftM2B.write(1)
-        self.rightM1A.write(1)
-        self.rightM2B.write(1)
+        GPIO.output(self.leftM1APin,1)
+        GPIO.output(self.leftM2APin,1)
+        GPIO.output(self.rightM1BPin,1)
+        GPIO.output(self.rightM2BPin,1)
+
         time.sleep(delay)
         self.stop() if willStop else None
 
     def clockwise(self,delay=5,willStop=True):
-        self.leftM1A.write(1)
-        self.leftM2A.write(1)
-        self.rightM1B.write(1)
-        self.rightM2B.write(1)
-        time.sleep(delay)
+        GPIO.output(self.leftM1APin,1)
+        GPIO.output(self.leftM2BPin,1)
+        GPIO.output(self.rightM1BPin,1)
+        GPIO.output(self.rightM2APin,1)
+
+        time.sleep(delay)       
         self.stop() if willStop else None
 
     def counter_clockwise(self,delay=5,willStop=True):
-        self.leftM1B.write(1)
-        self.leftM2B.write(1)
-        self.rightM1A.write(1)
-        self.rightM2A.write(1)
+        GPIO.output(self.leftM1BPin,1)
+        GPIO.output(self.leftM2APin,1)
+        GPIO.output(self.rightM1APin,1)
+        GPIO.output(self.rightM2BPin,1)
+
         time.sleep(delay)
         self.stop() if willStop else None
-    
-    def readPin(self,data):
-        self.pin11Data=data
-    
-    def getDistance(self):
-        print("getting distance")
-        self.ultrasonicTrigg.write(1)
-        time.sleep(0.00001)
-        self.ultrasonicTrigg.write(0)
         
-        while self.pin11Data==0:
-            self.pulse_start=time.time()
-        while self.pin11Data==1:
-            self.pulse_end=time.time()
-        self.pulseDuration = self.pulse_end-self.pulse_start
-        self.dist = self.pulseDuration*34300/2
-        return self.dist
+    # def getDistance(self):
+    #         dist=None
+    #     # while dist==None:
+    #         count=0
+    #         print("getting dist")
+    #         GPIO.output(self.Trig, GPIO.HIGH)
+    #         time.sleep(0.00001)
+    #         GPIO.output(self.Trig, GPIO.LOW)
+    #         pulse_start=time.time()
+    #         pulse_end=time.time()
+    #         ls=[]
+    #         while GPIO.input(self.Echo)==0:
+    #             pulse_start = time.time()
+    #             count+=1
+    #             ls.append(pulse_start)
+    #             if count>300:
+    #                 break
+    #             # GPIO.output(self.Trig, GPIO.HIGH)
+    #             # time.sleep(0.00001)
+    #             # GPIO.output(self.Trig, GPIO.LOW)
+            
+    #         while GPIO.input(self.Echo)==1:
+    #             pulse_end = time.time()
+
+    #         pulseDuration = pulse_end-ls[-2]
+    #         dist = pulseDuration*34300/2
+    #         return dist
         
-    def servoWrite(self,value,delay=2):
-        self.alignmentServo.write(value)
-        print("writing servo")
-        time.sleep(delay)
+    def servoWrite(self,angle):
+        angle = self.num_to_range(angle, 0,100,270,360)
+        print(angle)
+        self.servoCam.value = math.sin(math.radians(angle))
+
+    def __init__(self,  servo_pin=12, leftM1APin=14, leftM1BPin=15, leftM2APin=18, leftM2BPin=5, rightM1APin=6,
+                 rightM1BPin=7, rightM2APin=8, rightM2BPin=9, Trig=10, Echo=11):  
         
-    def __init__(self, com='/dev/ttyACM0', servo_pin=12, leftM1APin=2, leftM1BPin=3, leftM2APin=4, leftM2BPin=5, rightM1APin=6,
-                 rightM1BPin=7, rightM2APin=8, rightM2BPin=9, Trig=10, Echo=11, interval=19, printLog=True,type="arduino nano"):  
+        print("setup rpi...")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         
-        print("initializing arduino...")
-        if type=="arduino nano":
-            self.arduino = pyfirmata2.ArduinoNano(com)
-        elif type=="arduino":
-            self.arduino = pyfirmata2.Arduino(com)
-        else:
-            print("no type of boards mentioned found")
-        self.arduino.samplingOn(interval)
-        
-        print("arduino initialized, starting setup...")
-        self.alignmentServo = self.arduino.get_pin(f"d:{servo_pin}:s")
-        
-        #setup left side dc motor
-        self.leftM1A=self.arduino.get_pin(f"d:{leftM1APin}:o")
-        self.leftM1B=self.arduino.get_pin(f"d:{leftM1BPin}:o")
-        self.leftM2A=self.arduino.get_pin(f"d:{leftM2APin}:o")
-        self.leftM2B=self.arduino.get_pin(f"d:{leftM2BPin}:o")
-        self.leftM1A.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.leftM1B.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.leftM2A.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.leftM2B.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
+        print("initialized, starting setup...") #havent setup servo
+        self.leftM1APin=leftM1APin
+        self.leftM1BPin=leftM1BPin
+        self.leftM2APin=leftM2APin
+        self.leftM2BPin=leftM2BPin
+        self.rightM1APin=rightM1APin
+        self.rightM1BPin=rightM1BPin
+        self.rightM2APin=rightM2APin
+        self.rightM2BPin=rightM2BPin
+        self.Trig=Trig
+        self.Echo=Echo
+        #the camera servo
+        factory = PiGPIOFactory()
+        self.servoCam = Servo(servo_pin, pin_factory=factory, min_pulse_width=(0.5/1000), max_pulse_width=(2.5/1000))
+        self.servoCam.value = None
+
+        #setup ultrasonic
+        GPIO.setup(self.Trig, GPIO.OUT)
+        GPIO.setup(self.Echo, GPIO.IN)
+
+        #setup self.leftM1BPin side dc motor
+        GPIO.setup(self.leftM1APin,GPIO.OUT)
+        GPIO.setup(self.leftM1BPin,GPIO.OUT)
+        GPIO.setup(self.leftM2APin,GPIO.OUT)
+        GPIO.setup(self.leftM2BPin,GPIO.OUT)
 
         #setup right side dc motor
-        self.rightM1A=self.arduino.get_pin(f"d:{rightM1APin}:o")
-        self.rightM1B=self.arduino.get_pin(f"d:{rightM1BPin}:o")
-        self.rightM2A=self.arduino.get_pin(f"d:{rightM2APin}:o")
-        self.rightM2B=self.arduino.get_pin(f"d:{rightM2BPin}:o")
-        self.rightM1A.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.rightM1B.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.rightM2A.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
-        self.rightM2B.register_callback(lambda value:print("Button released") if value and printLog else print("Button pressed"))
+        GPIO.setup(self.rightM1APin, GPIO.OUT)
+        GPIO.setup(self.rightM1BPin, GPIO.OUT)
+        GPIO.setup(self.rightM2APin, GPIO.OUT)
+        GPIO.setup(self.rightM2BPin, GPIO.OUT)
         
-        #setup ultrasonic
-        self.ultrasonicTrigg=self.arduino.get_pin(f"d:{Trig}:o")
-        self.ultrasonicEcho=self.arduino.get_pin(f"d:{Echo}:i")
-        self.ultrasonicTrigg.register_callback(lambda value:print("ultrasound sent") if value and printLog else None)
-        self.ultrasonicEcho.register_callback(self.readPin)
-        self.ultrasonicEcho.enable_reporting()
-        self.pin11Data=0
+        
+        time.sleep(2)
         
         print("finished setting up. moving onto next phase")
+
+    def num_to_range(self,num, inMin, inMax, outMin, outMax):
+        return int(outMin + (float(num - inMin) / float(inMax - inMin) * (outMax
+                        - outMin)))
 
     def testRunMotor(self,tDelay=2):
         print("running tests on DC motors...")
@@ -149,17 +171,18 @@ class board():
         print("turning counter-clockwise")
         self.counter_clockwise(delay=tDelay)
         
-        print("testing ultrasonic sensor...")
-        dist = self.getDistance()
-        print(f"distance: {dist}")
+        # print("testing ultrasonic sensor...")
+        # dist = self.getDistance()
+        # print(f"distance: {dist}")
         
         print("testing servo")
-        self.servoWrite(0)
-        self.servoWrite(180)
+        self.servoWrite(1)
+        time.sleep(1)
+        self.servoWrite(90)
         
         print("test run complete! All hardware modules working.")
 
 if __name__ == "__main__":
-    nano = board(com="COM8")
-    nano.testRunDCMotor()
-
+    nano = car()
+    nano.stop()
+    nano.testRunMotor()
