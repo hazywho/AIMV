a3c2d2cef64cbb6da00a12e9ce8a7273491614ef
hazywho <zanyihii2468@gmail.com>
2024-11-27 10:12:25 +0800

    lol

--- FIRMATA+RPI/Internet/server/compute.py
+++ FIRMATA+RPI/Internet/server/compute.py
@@ -38,7 +38,7 @@ class cloud():
         #precompute some values
         frame=self.request()
         prediction = self.model.predict(source=frame,stream_buffer=False, classes=[0],verbose=False)
-        exists=1
+        exists=0
         if prediction[0]:
             #declare and assign variables
             totalBoxes = sorted(list(prediction[0].boxes.xyxy),key=self.getBiggest,reverse=True)
@@ -49,30 +49,13 @@ class cloud():
             midpoint = (midpoint[0]+abs(round((boxes[2]-boxes[0])/2)),
                         midpoint[1]+abs(round((boxes[3]-boxes[1])/2)))
             
-            #putting boxes on detrected items
-            cv2.rectangle(frame, (boxes[0],boxes[1]),(boxes[2],boxes[3]),color=(255,0,0),thickness=2)
-
-            #mark edge boxes
-            cv2.rectangle(frame,(self.bottomHeight,self.topHeight),(self.topWidth,self.bottomWidth),color=(255,0,0),thickness=1) 
-            
-            #y top/bottom limit
-            frame = cv2.line(frame, (0,self.limitTop), (round(self.width),self.limitTop), (0,255,0), 1)
-            frame = cv2.line(frame, (0,self.limitBottom), (round(self.width),self.limitBottom), (0,255,0), 1)
-            #x left/right limit
-            frame = cv2.line(frame, (self.limitLeft,0), (self.limitLeft,round(self.height)), (0,255,0), 1)
-            frame = cv2.line(frame, (self.limitRight,0), (self.limitRight,round(self.height)), (0,255,0), 1)
-            #midpoiconfidence
-            frame = cv2.circle(frame, (midpoint[0],midpoint[1]), radius=10, color=(0, 0, 255), thickness=-1)
-            cv2.putText(frame,f"{classes}, {confidence}",(boxes[0],boxes[1]-10),cv2.FONT_HERSHEY_COMPLEX,1,color=(0,0,0),thickness=2,lineType=cv2.LINE_AA)
-            
             #calculate servo angles
             self.servoXlimit=100
             self.servoYlimit=90
-            exists=0
-            return [[exists,exists],[self.limitBottom,self.limitTop], [self.limitLeft, self.limitRight],[midpoint[0],midpoint[1]]] 
+            exists=1
+            return [[exists,exists],[self.limitBottom,self.limitTop], [self.limitLeft, self.limitRight],[midpoint[0],midpoint[1]],[boxes[0],boxes[1]],[boxes[2],boxes[3]],[confidence,classes]] 
         else:
-            return [[0,0],[self.limitBottom,self.limitTop], [self.limitLeft, self.limitRight],[None,(self.limitTop-self.limitBottom)/2]]
-        
+            return [[0,0],[self.limitBottom,self.limitTop], [self.limitLeft, self.limitRight],[None,(self.limitTop-self.limitBottom)/2],[0,0],[0,0],[0,0]]
 
 #run this only when the code in the car is ran     
 if __name__=="__main__":
