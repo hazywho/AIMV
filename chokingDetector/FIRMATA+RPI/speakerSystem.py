83d20b8153749b1e00f3e40341e2785eca26b621
hazywho <zanyihii2468@gmail.com>
2024-11-27 14:43:43 +0800

    n

---
+++ chokingDetector/FIRMATA+RPI/speakerSystem.py
@@ -0,0 +1,21 @@
+import simpleaudio as sa
+
+class speaker():
+    
+    def playAudio(self,waitDone=False,path = r"C:\Users\zanyi\Documents\GitHub\AIMV\chaseAtlantic.wav"):
+        self.f=sa.WaveObject.from_wave_file(path)
+        self.play_object = self.f.play()
+        self.play_object.wait_done() if waitDone else None
+        
+    def stopAudio(self):
+        self.play_object.stop()
+
+import time
+if __name__ == "__main__":
+    audioMachine = speaker()
+    audioMachine.playAudio()
+    print("asyncronus playing is working")
+    for n in range(20):
+        print(f"testing {n}") 
+    time.sleep(5)
+    audioMachine.stopAudio()
